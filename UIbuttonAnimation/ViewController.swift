7567dcce1c5bbf6504024e0dc09310ae64a3edab
LIANG ZHAO <liangzhao@node-1w7jr9qqdi59vp46y5a37hedu.ipv6.telus.net>
2017-10-26 20:10:35 -0700

    Initial Commit

---
+++ UIbuttonAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIbuttonAnimation
+//
+//  Created by LIANG ZHAO on 2017-10-26.
+//  Copyright © 2017 bcit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
